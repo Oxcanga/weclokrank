3a9a1c41ff4d4b17f9731b1de7171f32ef7bc8fb
Oxcanga <ilgcreatny@gmail.com>
2024-01-01 13:28:17 +0300

    KodLand Runner Paketi İndirildi Ve Bazı Değişiklikler
    *Kodland Runner Paketi İndi Çünkü Animasyonlar Paketin İçerisinde
    *CharacterScript Güncellendi
    *Karaktere Yürüme Animasyonu Bağlandı Fakat Bazı Buglar Var
    *Yeni Bir UI Animasyonu Oluşturuldu (AnimationsTest) Sahnesinde
    -

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,6 +4,16 @@ public class CharacterScript : MonoBehaviour
 {
     public float speed = 5f;
     public Camera camera;
+    [SerializeField] private RuntimeAnimatorController animatorController; // AnimatorController nesnesini SerializeField olarak tanımladık
+
+    private Animator animator; // Animator nesnesi ekledik
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); // Animator nesnesini başlatıyoruz
+        animator.runtimeAnimatorController = animatorController; // AnimatorController'ı atıyoruz
+    }
+
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -11,6 +21,15 @@ public class CharacterScript : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
         transform.Translate(movement);
-        
+
+        // Animasyonları kontrol ediyoruz
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("isMoving", true); // Eğer karakter hareket ediyorsa "isMoving" animasyonunu başlatıyoruz
+        }
+        else
+        {
+            animator.SetBool("isMoving", false); // Eğer karakter hareket etmiyorsa "isMoving" animasyonunu durduruyoruz
+        }
     }
 }
